ddcb2c87450ffb88fef356cd40f318fd4e71f654
moekaz <mohamed.kazma@carleton.ca>
2019-05-03 16:21:58 -0400

    some mini fixes

--- Arcane Engine Core/src/input/JoystickInputData.h
+++ Arcane Engine Core/src/input/JoystickInputData.h
@@ -1,9 +1,3 @@
-/*
-	Author: Mohamed Kazma
-	Descriptions: Holds joystick data and handles events sent from the joystick manager via glfw
-	and updates the corresponding data it is holding
-*/
-
 #pragma once
 
 #define MAX_JOYSTICK_BUTTONS 18
@@ -34,45 +28,29 @@ namespace arcane
 	class JoystickInputData
 	{
 	public:
-		// Store the state of each joytick button to be queried
 		unsigned char m_ButtonStates[MAX_JOYSTICK_BUTTONS];
 
 		JoystickInputData(char id = 0);
 		~JoystickInputData();
 
-		// Update the joystick's connection status
 		inline void SetConnection(bool isConnected) { m_Connected = isConnected; }
-		// Set the joystick id that we want to query
 		inline void SetId(char id) { m_Id = id; }
-		// Set the deadzone of the joystick
 		inline void SetDeadzone(float dead) { m_Deadzone = dead; }
-		// Get the number of buttons that this joystick has
 		inline char GetNumButtons() { return m_NumButtons; }
-		// Get the left stick values
 		inline glm::vec2& GetLeftStick() { return m_LeftStick; }
-		// Get Right stick values
 		inline glm::vec2& GetRightStick() { return m_RightStick; }
-		// Get Triggers
 		inline glm::vec2& GetTriggers() { return m_Triggers; }
-		// Check if the joystick is connected
 		inline bool IsConnected() { return m_Connected; }
 
-		// Update the joystick values by querying for button states
 		void Update();
 
 	private:
-		// Is the joystick connected
 		bool m_Connected; 
-		// The id that we will use to query for button states
 		char m_Id; 
-		// The number of buttons is different between controllers
 		int m_NumButtons; 
-		// DeadZone value for the analog buttons (range 0-1)
 		float m_Deadzone;
-		// Axis positions of the left and right analog sticks
 		glm::vec2 m_LeftStick;
 		glm::vec2 m_RightStick;
-		// Values of the left and right triggers
 		glm::vec2 m_Triggers;
 	};
 }
