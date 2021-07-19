70123021a8c87578e02f9f8e007f1965c38defbd
Ershany <bradyjessup@hotmail.com>
2021-07-19 18:45:40 -0400

    Updated comments

--- Arcane Editor/src/EditorLayer.cpp
+++ Arcane Editor/src/EditorLayer.cpp
@@ -29,7 +29,21 @@ namespace Arcane
 
 	void EditorLayer::OnImGuiRender()
 	{
+		static bool openBool = true;
 
+		ImGuiIO& io = ImGui::GetIO();
+		ImGuiStyle& style = ImGui::GetStyle();
+
+		ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+		ImGui::Begin("DockSpace Demo", &openBool, window_flags);
+
+		// Setup dock space
+		if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+		{
+			ImGuiID dockspaceID = ImGui::GetID("DockingSpace");
+		}
+
+		ImGui::End();
 	}
 
 	void EditorLayer::OnEvent(Event &event)
