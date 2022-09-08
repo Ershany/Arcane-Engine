8a3c8bff4dd7df992925b10f395c66481dec5946
Ershany <bradyjessup@hotmail.com>
2022-09-07 21:44:59 -0400

    Added some premake files. Not complete

---
+++ Arcane/premake5.lua
@@ -0,0 +1,39 @@
+project "Arcane"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "off"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "arcpch.h"
+	pchsource "src/arcpch.cpp"
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp"
+	}
+
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS",
+		"GLEW_STATIC"
+	}
+
+	includedirs
+	{
+		"src",
+		
+	}
+
+	links
+	{
+		"GLFW",
+		"GLEW",
+		"SOIL",
+		"Assimp",
+		"FreeType",
+		"FreeType-GL"
+	}
