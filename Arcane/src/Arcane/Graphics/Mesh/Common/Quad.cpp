979f2cfff2bb0649d30c8fe8c02d4c84183f6f62
Ershany <bradyjessup@hotmail.com>
2023-03-21 17:59:08 -0400

    Cleaned up images for the different light sources

--- Arcane/src/Arcane/Graphics/Mesh/Common/Quad.cpp
+++ Arcane/src/Arcane/Graphics/Mesh/Common/Quad.cpp
@@ -13,10 +13,10 @@ namespace Arcane
 		}
 		else
 		{
-			m_Positions.push_back(glm::vec3(-0.5f, 0.5f, 0));  m_UVs.push_back(glm::vec2(0, 1)); m_Normals.push_back(glm::vec3(0, 0, 1)); m_Tangents.push_back(glm::vec3(1, 0, 0)); m_Bitangents.push_back(glm::vec3(0, 1, 0));
-			m_Positions.push_back(glm::vec3(0.5f, 0.5f, 0));   m_UVs.push_back(glm::vec2(1, 1)); m_Normals.push_back(glm::vec3(0, 0, 1)); m_Tangents.push_back(glm::vec3(1, 0, 0)); m_Bitangents.push_back(glm::vec3(0, 1, 0));
-			m_Positions.push_back(glm::vec3(-0.5f, -0.5f, 0)); m_UVs.push_back(glm::vec2(0, 0)); m_Normals.push_back(glm::vec3(0, 0, 1)); m_Tangents.push_back(glm::vec3(1, 0, 0)); m_Bitangents.push_back(glm::vec3(0, 1, 0));
-			m_Positions.push_back(glm::vec3(0.5f, -0.5f, 0));  m_UVs.push_back(glm::vec2(1, 0)); m_Normals.push_back(glm::vec3(0, 0, 1)); m_Tangents.push_back(glm::vec3(1, 0, 0)); m_Bitangents.push_back(glm::vec3(0, 1, 0));
+			m_Positions.push_back(glm::vec3(-0.5f, 0.5f, 0));  m_UVs.push_back(glm::vec2(0, 0)); m_Normals.push_back(glm::vec3(0, 0, 1)); m_Tangents.push_back(glm::vec3(1, 0, 0)); m_Bitangents.push_back(glm::vec3(0, 1, 0));
+			m_Positions.push_back(glm::vec3(0.5f, 0.5f, 0));   m_UVs.push_back(glm::vec2(1, 0)); m_Normals.push_back(glm::vec3(0, 0, 1)); m_Tangents.push_back(glm::vec3(1, 0, 0)); m_Bitangents.push_back(glm::vec3(0, 1, 0));
+			m_Positions.push_back(glm::vec3(-0.5f, -0.5f, 0)); m_UVs.push_back(glm::vec2(0, 1)); m_Normals.push_back(glm::vec3(0, 0, 1)); m_Tangents.push_back(glm::vec3(1, 0, 0)); m_Bitangents.push_back(glm::vec3(0, 1, 0));
+			m_Positions.push_back(glm::vec3(0.5f, -0.5f, 0));  m_UVs.push_back(glm::vec2(1, 1)); m_Normals.push_back(glm::vec3(0, 0, 1)); m_Tangents.push_back(glm::vec3(1, 0, 0)); m_Bitangents.push_back(glm::vec3(0, 1, 0));
 		}
 
 		m_Indices.push_back(1); m_Indices.push_back(0); m_Indices.push_back(2);
